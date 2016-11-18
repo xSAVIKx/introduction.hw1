49d0dd762d0af6806a79a36ecfa578fa460e4ea9
Iurii Sergiichuk <iurii.sergiichuk@teamdev.com>
2016-11-18 13:32:43 +0200

    update

--- src/main/java/school/lemon/changerequest/java/introduction/hw1/Task4.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw1/Task4.java
@@ -4,13 +4,6 @@ package school.lemon.changerequest.java.introduction.hw1;
 public class Task4 {
 
     public static double getSumOfGreatest(double a, double b, double c) {
-        if (a > b && b > c) {
-            return a + b;
-        }
-        if (a > b && b < c) {
-            return a + c;
-        }
-        return b + c;
-
+        return 0;
     }
 }
